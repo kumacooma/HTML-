4da8e12683d9db1a6c75a4128928a76b64104f8f
kumacooma <cooperbook123@naver.com>
2024-01-23 13:32:52 +0900

    project file

--- 라온이.js
+++ 라온이.js
@@ -23,8 +23,8 @@ function openModal() {
     let userInput = document.getElementById("userInput").value;
 
     if(userInput){
-    // 모달 창을 보이도록 설정
-    document.getElementById("myModal3").style.display = "flex";}
+        // 모달 창을 보이도록 설정
+        document.getElementById("myModal3").style.display = "flex";}
     else{
         document.getElementById("myModal4").style.display = "flex";
     }
@@ -44,28 +44,66 @@ function refreshPage() {
 
 
 
-function openLightbox(imageSrc, downloadLink) {
+// function openLightbox(imageSrc, downloadLink) {
+//     // Lightbox 표시
+//     let lightbox = document.getElementById('lightbox');
+//     lightbox.style.display = 'flex';
+//     let overlay = document.getElementById('overlay');
+//     overlay.style.display = 'block';
+//     // Lightbox 이미지 업데이트
+//     let lightboxImage = document.getElementById('lightboxImage');
+//     lightboxImage.src = imageSrc;
+//
+//     // Lightbox 다운로드 링크 업데이트
+//     let downloadButton = document.getElementById('downloadLink');
+//     downloadButton.href = downloadLink;
+// }
+//
+//
+//
+// function closeLightbox() {
+//     // Lightbox 닫기
+//     let lightbox = document.getElementById('lightbox');
+//     lightbox.style.display = 'none';
+//     let overlay = document.getElementById('overlay');
+//     overlay.style.display = 'none';
+// }
+
+
+function openLightbox(imageSrc, downloadLink, lightboxId) {
     // Lightbox 표시
-    let lightbox = document.getElementById('lightbox');
+    let lightbox = document.getElementById(lightboxId);
     lightbox.style.display = 'flex';
+
     let overlay = document.getElementById('overlay');
     overlay.style.display = 'block';
+
     // Lightbox 이미지 업데이트
-    let lightboxImage = document.getElementById('lightboxImage');
+    let lightboxImage = lightbox.querySelector('.lightboxImage');
     lightboxImage.src = imageSrc;
 
     // Lightbox 다운로드 링크 업데이트
-    let downloadButton = document.getElementById('downloadLink');
+    let downloadButton = lightbox.querySelector('.사진다운버튼링크');
     downloadButton.href = downloadLink;
 }
 
-
-
-function closeLightbox() {
+function closeLightbox(lightboxId) {
     // Lightbox 닫기
-    let lightbox = document.getElementById('lightbox');
+    let lightbox = document.getElementById(lightboxId);
     lightbox.style.display = 'none';
     let overlay = document.getElementById('overlay');
     overlay.style.display = 'none';
 }
+
+function closeAllLightboxes() {
+    // 모든 Lightbox 닫기
+    let lightboxes = document.querySelectorAll('.lightbox');
+    for (let i = 0; i < lightboxes.length; i++) {
+        lightboxes[i].style.display = 'none';
+    }
+
+    let overlay = document.getElementById('overlay');
+    overlay.style.display = 'none';
+}
+
 
